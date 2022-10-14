d358f6f98efa8ab950d34ec7166c80de71e20352
Treehays90 <33053528+Treehays90@users.noreply.github.com>
2022-10-14 23:11:13 +0100

    Sale Managment System First commit

---
+++ SMS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SMSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+

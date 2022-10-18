31f57813032c136cad849b37a0a399473d7bfecd
Treehays90 <33053528+Treehays90@users.noreply.github.com>
2022-10-18 07:30:52 +0100

    Assumingworking after editing properties of customer

--- SMS/model/Customer.cs
+++ SMS/model/Customer.cs
@@ -1,19 +1,26 @@
 namespace SMS.model
 {
-    public class Customer : User
+    public class Customer
     {
-        public int Id {get;set;}
+        public int Id { get; set; }
         public string Address { get; set; }
         public string CustomerId { get; set; }
         public double Wallet { get; set; }
-        public string FirstName;
-        public string LastName;
-        public string Email;
-         public string phoneNumber;
-         public int Pin;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Pin { get; set; }
         // public Customer(int id, string firstName, string lastName, int staffId, string email, string phoneNumber, int pin, string post, string address, string dateOfBirth, int age, double wallet) : base(id, firstName, lastName, staffId, email, phoneNumber, pin, dateOfBirth, age)
-        public Customer(int id, string firstName, string lastName, string email, string customerId, string phoneNumber, int pin, string address, double wallet);
+        public Customer(int id, string firstName, string lastName, string email, string customerId, string phoneNumber, int pin, string address, double wallet)
         {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            CustomerId = customerId;
+            PhoneNumber = phoneNumber;
+            Pin = pin;
             Address = address;
             Wallet = wallet;
         }
